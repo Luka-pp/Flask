343467f59fa8a2177d157e94f2545d3ac20df6c5
luka_pp <luka_pp@hotmail.com>
2021-04-10 10:35:26 +0100

    updated app.py

--- app.py
+++ app.py
@@ -48,4 +48,7 @@ def careers():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
